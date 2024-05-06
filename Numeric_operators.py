7c95d44f26db5a68c324a3fb128cec7c06b8999b
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-05-06 15:23:39 +0530

    Add files via upload

---
+++ Numeric_operators.py
@@ -0,0 +1,25 @@
+# Numeric operations
+a=21
+b=10
+
+# addition
+print("a+b:",a+b)
+
+# subtraction
+print("a-b:",a-b)
+
+# multiplication
+print("a*b:",a*b)
+
+# division
+print("a/b:",a/b)
+
+# exponent
+print("a**b:",a**b)
+
+# modulus
+print("a%b:",a%b)
+
+# floor division
+print("a//b:",a//b)
+
